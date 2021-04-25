2541d2af397f1264bcf09cdd0029f726682f68a2
Neveh Allon <34620850+nevehallon@users.noreply.github.com>
2021-04-25 17:16:40 +0300

    Add: view and edit user information

--- src/port/components/pages/acount/myProfile.tsx
+++ src/port/components/pages/acount/myProfile.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-max-props-per-line */
 import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
-import { useHistory } from "react-router";
+import { useHistory } from "react-router-dom";
 
 import { Box, Container, Grid } from "@material-ui/core";
 
